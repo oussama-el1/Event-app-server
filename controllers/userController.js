e4a663c2221712aec8c7571a11df110b464badee
oussama-el1 <elhadraoui284@gmail.com>
2024-08-23 17:32:49 +0100

    some edit in user Route

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,7 @@ class UserController {
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
+  };
 
   static async getUser(req, res) {
     try {
@@ -41,7 +41,7 @@ class UserController {
     } catch (error) {
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
+  };
   
   /**
    * The magic of Update a User  
@@ -113,7 +113,7 @@ class UserController {
       console.error(`Error updating user: ${error.message}`);
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
+  };
 
   static async followUser(req, res) {
     try {
@@ -150,7 +150,7 @@ class UserController {
       console.error('Error following user:', error);
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
+  };
 
   static async unfollowUser(req, res) {
     const currentUserId = req.user.id;
@@ -187,7 +187,7 @@ class UserController {
       console.error(`Error unfollowing user: ${error.message}`);
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
+  };
 
   static async getFollowers(req, res) {
     const userId = req.user.id;
@@ -198,14 +198,13 @@ class UserController {
       }
       
       const followers = user.followers.map(follower => follower.toObject());
-      
+
       res.status(200).json(followers);
     } catch (error) {
       console.error(`Error getting followers: ${error.message}`);
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
-  
+  };
 
   static async getFollowings(req, res) {
     const userId = req.user.id;
@@ -220,37 +219,7 @@ class UserController {
       console.error(`Error getting following: ${error.message}`);
       res.status(500).json({ message: 'Internal server error' });
     }
-  }
-
-  static async getFollowersCount(req, res) {
-    const userId = req.user.id;
-    try {
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      const followersCount = user.followers.length;
-      res.status(200).json({ followersCount });
-    } catch (error) {
-      console.error(`Error getting followers count: ${error.message}`);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  }
-
-  static async getFollowingCount(req, res) {
-    const userId = req.user.id;
-    try {
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      const followingCount = user.following.length;
-      res.status(200).json({ followingCount });
-    } catch (error) {
-      console.error(`Error getting following count: ${error.message}`);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  }
+  };
 
   /**
    * Search users
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const UserController = require('../controllers/userController')
-const ProtectMidll = require('../middlewares/Authenticated')
+const UserController = require('../controllers/userController');
+const ProtectMidll = require('../middlewares/Authenticated');
 const upload = require('../middlewares/uploadImage');
 
 const UserRouter = express.Router();
 
-UserRouter.get('/events', ProtectMidll, UserController.getEvents);
-UserRouter.get('/bookings', ProtectMidll, UserController.getBookings);
-
 UserRouter.get('/me', ProtectMidll, UserController.getMe);
-UserRouter.get('/:id', ProtectMidll, UserController.getUser);
 UserRouter.put('/me', ProtectMidll, upload.single('profilePicture'), UserController.updateMe);
 UserRouter.post('/follow', ProtectMidll, UserController.followUser);
 UserRouter.post('/unfollow', ProtectMidll, UserController.unfollowUser);
 UserRouter.get('/followers', ProtectMidll, UserController.getFollowers);
 UserRouter.get('/followings', ProtectMidll, UserController.getFollowings);
-UserRouter.get('/followers-count', ProtectMidll, UserController.getFollowersCount);
-UserRouter.get('/following-count', ProtectMidll, UserController.getFollowingCount);
 UserRouter.get('/search', ProtectMidll, UserController.searchUsers);
+UserRouter.get('/events', ProtectMidll, UserController.getEvents);
+UserRouter.get('/bookings', ProtectMidll, UserController.getBookings);
+
+// get a specifique user profile
+UserRouter.get('/:id', ProtectMidll, UserController.getUser);
 
 module.exports = UserRouter;
